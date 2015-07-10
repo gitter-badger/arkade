93b090b7de2ea5ee5fb7786efad4b3768fe86a3c
Felix Angell <mail@felixangell.com>
2015-07-10 13:06:07 +0100

    should initialize a simple git repo + gitignore

--- includes/actions.h
+++ includes/actions.h
@@ -3,11 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define CONFIG_NAME ("Ark.toml")
+#define CONFIG_NAME "Ark.toml"
+#define DEPENDENCY_FOLDER_NAME "_deps"
+#define SRC_FOLDER_NAME "src"
 
 void help_action();
 void new_action();
--- src/actions.c
+++ src/actions.c
@@ -24,6 +24,22 @@ static void create_directory(const char *path, int mode) {
     }
 }
 
+static FILE *create_file(const char *name) {
+    FILE *handle = fopen(name, "rb+");
+    if (!handle) {
+        handle = fopen(name, "wb");
+        if (!handle) {
+            printf("error: could not create file `%s`\n", name);
+            return false;
+        }
+    } 
+    else {
+        printf("error: could not create file `%s`\n", name);
+        return false;
+    }
+    return handle;
+}
+
 void help_action() {
     // todo we can iterate through our
     // command/args list?
@@ -31,37 +47,45 @@ void help_action() {
 }
 
 void new_action() {
+    const char *package_name = "Onion Man";
+    const char *package_author = "John Terry";
+    const char *package_version = "0.0.1";
+    const char *package_author_email = "terry@terry.cat";
+
     // create our deps folder
-    create_directory("_deps", 0700);
-    create_directory("src", 0700);
+    create_directory(DEPENDENCY_FOLDER_NAME, 0700);
+    create_directory(SRC_FOLDER_NAME, 0700);
 
-    FILE *handle = fopen(CONFIG_NAME, "rb+");
-    if (!handle) {
-        handle = fopen(CONFIG_NAME, "wb");
-        if (!handle) {
-            printf("error: could not create config file\n");
-            return;
-        }
+    // create basic config file
+    FILE *config_file = create_file(CONFIG_NAME);
+    fprintf(config_file, "[package]\n");
+    fprintf(config_file, "name = %s\n", package_name);
+    fprintf(config_file, "version = %s\n", package_version);
+    fprintf(config_file, "authors = [\n");
+    fprintf(config_file, "\t\"%s <%s>\"\n", package_author, package_author_email);
+    fprintf(config_file, "]\n");
+    fclose(config_file);
 
-        // todo load stuff from git/read or just read??
-        const char *package_name = "Onion Man";
-        const char *package_author = "John Terry";
-        const char *package_version = "0.0.1";
-        const char *package_author_email = "terry@terry.cat";
+    FILE *gitignore = create_file(".gitignore");
+    fprintf(gitignore, "_deps/\n");     // dependencies
+    fprintf(gitignore, "*.s/\n");       // asm
+    fprintf(gitignore, "*.ll/\n");      // llvm-ir
+    fprintf(gitignore, "*.o/\n");       // objects
+    fprintf(gitignore, "*.bc/\n");      // llvm bitcode
+    fclose(gitignore);
 
-        fprintf(handle, "[package]\n");
-        fprintf(handle, "name = %s\n", package_name);
-        fprintf(handle, "version = %s\n", package_version);
-        fprintf(handle, "authors = [\n");
-        fprintf(handle, "\t\"%s <%s>\"\n", package_author, package_author_email);
-        fprintf(handle, "]\n");
-        fclose(handle);
-    } 
-    else {
-        printf("error: could not create config file\n");
-    }
+    // setup git repository
+    // TODO dont use system calls
+    // but for now it works
+    system("git init");
+    system("git add --all");
+    system("git commit -m \"created new project with arkade!\"");
 }
 
 void publish_action() {
-    
+    // create repository on github
+    // push repository
+    // TODO fuck with curl to send create repo request
+    // add a remote, maybe diff name than origin
+    // push to remote
 }
