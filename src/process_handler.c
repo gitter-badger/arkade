88fd2734046ae61395fbda7aba3b85c0d2e1a9bf
Felix Angell <mail@felixangell.com>
2015-07-16 02:09:44 +0100

    removed un-necessary shit

--- src/process_handler.c
+++ src/process_handler.c
@@ -1,13 +1,6 @@
 #include "process_handler.h"
 
-static int max_process_count = 10;
-static int num_of_processes = 0;
-
 bool __create_process(char *command, char *input, ...) {
-    if (num_of_processes >= max_process_count) {
-        printf("yo, wait the fuck up\n");
-        return false;
-    }
     process_t proc;
     proc.output = NULL;
     proc.failed = false;
