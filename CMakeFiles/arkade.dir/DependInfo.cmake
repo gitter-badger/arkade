dc5e74598d641d7308091fc5e478bd6d71a67205
Vedant <nevetia.vedant@gmail.com>
2015-07-17 12:21:04 +0530

    add travis CI

---
+++ CMakeFiles/arkade.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/electricshepherd/Work/arkade/src/actions/build_action.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/actions/build_action.c.o"
+  "/Users/electricshepherd/Work/arkade/src/actions/help_action.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/actions/help_action.c.o"
+  "/Users/electricshepherd/Work/arkade/src/actions/login_action.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/actions/login_action.c.o"
+  "/Users/electricshepherd/Work/arkade/src/actions/new_action.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/actions/new_action.c.o"
+  "/Users/electricshepherd/Work/arkade/src/actions/publish_action.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/actions/publish_action.c.o"
+  "/Users/electricshepherd/Work/arkade/src/actions/update_action.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/actions/update_action.c.o"
+  "/Users/electricshepherd/Work/arkade/src/arguments.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/arguments.c.o"
+  "/Users/electricshepherd/Work/arkade/src/hashmap.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/hashmap.c.o"
+  "/Users/electricshepherd/Work/arkade/src/json_builder.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/json_builder.c.o"
+  "/Users/electricshepherd/Work/arkade/src/main.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/main.c.o"
+  "/Users/electricshepherd/Work/arkade/src/process_handler.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/process_handler.c.o"
+  "/Users/electricshepherd/Work/arkade/src/sds.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/sds.c.o"
+  "/Users/electricshepherd/Work/arkade/src/toml/ast.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/toml/ast.c.o"
+  "/Users/electricshepherd/Work/arkade/src/toml/lexer.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/toml/lexer.c.o"
+  "/Users/electricshepherd/Work/arkade/src/toml/load.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/toml/load.c.o"
+  "/Users/electricshepherd/Work/arkade/src/toml/parser.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/toml/parser.c.o"
+  "/Users/electricshepherd/Work/arkade/src/toml/sourcefile.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/toml/sourcefile.c.o"
+  "/Users/electricshepherd/Work/arkade/src/toml/token.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/toml/token.c.o"
+  "/Users/electricshepherd/Work/arkade/src/toml/toml.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/toml/toml.c.o"
+  "/Users/electricshepherd/Work/arkade/src/util.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/util.c.o"
+  "/Users/electricshepherd/Work/arkade/src/vector.c" "/Users/electricshepherd/Work/arkade/CMakeFiles/arkade.dir/src/vector.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "includes"
+  "includes/toml"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/arkade.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arkade.dir/src/arguments.c.o"
+  "CMakeFiles/arkade.dir/src/hashmap.c.o"
+  "CMakeFiles/arkade.dir/src/json_builder.c.o"
+  "CMakeFiles/arkade.dir/src/main.c.o"
+  "CMakeFiles/arkade.dir/src/process_handler.c.o"
+  "CMakeFiles/arkade.dir/src/sds.c.o"
+  "CMakeFiles/arkade.dir/src/util.c.o"
+  "CMakeFiles/arkade.dir/src/vector.c.o"
+  "CMakeFiles/arkade.dir/src/toml/ast.c.o"
+  "CMakeFiles/arkade.dir/src/toml/lexer.c.o"
+  "CMakeFiles/arkade.dir/src/toml/load.c.o"
+  "CMakeFiles/arkade.dir/src/toml/parser.c.o"
+  "CMakeFiles/arkade.dir/src/toml/sourcefile.c.o"
+  "CMakeFiles/arkade.dir/src/toml/token.c.o"
+  "CMakeFiles/arkade.dir/src/toml/toml.c.o"
+  "CMakeFiles/arkade.dir/src/actions/build_action.c.o"
+  "CMakeFiles/arkade.dir/src/actions/help_action.c.o"
+  "CMakeFiles/arkade.dir/src/actions/login_action.c.o"
+  "CMakeFiles/arkade.dir/src/actions/new_action.c.o"
+  "CMakeFiles/arkade.dir/src/actions/publish_action.c.o"
+  "CMakeFiles/arkade.dir/src/actions/update_action.c.o"
+  "bin/arkade.pdb"
+  "bin/arkade"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arkade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
